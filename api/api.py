dd4877205d69bcf5a28af6fe3ef525cdf36ec4e8
mosgreenhorn <girardi.dominic@gmail.com>
2018-12-06 12:13:07 +0100

    End day 2

--- api/api.py
+++ api/api.py
@@ -1,20 +1,37 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+import random
 
 app = Flask(__name__)
 api = Api(app)
 
 todos = {}
 
-class PVDataProvider(Resource):
+
+base_path = "/api/"
+
+class State(Resource):
     def get(self):
         return {"State": "online"}
 
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
 
-api.add_resource(PVDataProvider, '/')
+class PVDayDataProvider(Resource):
+    def get(self):
+        data = []
+        labels = []
+
+        for i in range(20):
+            data.append(i + random.randint(0,i))
+            labels.append(i)
+
+        return {
+            "data":data,
+            "labels":labels}
+
+
+
+api.add_resource(State, base_path)
+api.add_resource(PVDayDataProvider, base_path+'day')
 # api.add_resource()
 
 if __name__ == '__main__':
