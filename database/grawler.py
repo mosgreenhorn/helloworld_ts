91eb7f3d86660aea45f207e8893cad345b9e5001
mosgreenhorn <girardi.dominic@gmail.com>
2018-12-07 23:13:53 +0100

    End day 3

--- database/grawler.py
+++ database/grawler.py
@@ -1,6 +1,34 @@
 import requests
+import mysql.connector
+
 
 r = requests.get('http://192.168.8.200/solar_api/v1/GetPowerFlowRealtimeData.fcgi')
 data = r.json()
-print(data['Body']['Data']['Site']['P_PV'])
-print(data['Body']['Data']['Site']['E_Day'])
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="user",
+  passwd="P.assword123",
+  database="monitoring"
+)
+
+mycursor = mydb.cursor()
+
+
+sql = "INSERT INTO data (P_PV, E_Day, E_Year, E_Total) VALUES (%s, %s, %s, %s)"
+val = (
+    0 if data['Body']['Data']['Site']['P_PV'] is None else data['Body']['Data']['Site']['P_PV'], 
+    0 if data['Body']['Data']['Site']['E_Day'] is None else data['Body']['Data']['Site']['E_Day'], 
+    0 if data['Body']['Data']['Site']['E_Year'] is None else data['Body']['Data']['Site']['E_Year'], 
+    0 if data['Body']['Data']['Site']['E_Total'] is None else data['Body']['Data']['Site']['E_Total']
+)
+
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+mydb.close()
+
+
+
