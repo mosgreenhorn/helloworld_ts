e1949bdbae51f837f90583c335252364dc521ae6
mosgreenhorn <girardi.dominic@gmail.com>
2018-12-11 08:13:54 +0100

    Working on deployment automatisation

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 npm run build
 cp deployRemote.sh ../build/
-scp -r build/ greenhorn@192.168.8.202:/tmp/transfer
+cd ..
+scp -r build/* greenhorn@192.168.8.202:/tmp/transfer
 ssh greenhorn@192.168.8.202
--- deployment/deployRemote.sh
+++ deployment/deployRemote.sh
@@ -2,3 +2,8 @@
 sudo rm -rf /var/www/html/
 sudo mkdir /var/www/html
 sudo cp -a /tmp/transfer/* /var/www/html/
+rm -rf /tmp/transfer
+mkdir /tmp/transfer
+cd /home/greenhorn/workspace/helloworld_ts/
+git pull
+sudo shutdown -r -t 0
